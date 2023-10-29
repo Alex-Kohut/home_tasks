7853d896f6f2430a3ea8d807a337d01b0cb2fa9f
Alex-Kohut <oleksandrkohut@gmail.com>
2023-10-29 12:29:52 +0300

    home_tasks 7

---
+++ task7.py
@@ -0,0 +1,14 @@
+# lst = [0, 1, 0, 12, 3]
+# lst = [0]
+# lst = []
+# lst = [1, 0, 13, 0, 0, 0, 5]
+lst = [9, 0, 7, 31, 0, 45, 0, 45, 0, 45, 0, 0, 96, 0]
+
+el = 0
+for el in lst:
+    if  el == 0:
+        i = lst.index(el)
+        lst.pop(i)
+        lst.append(0)
+print(lst)
+
