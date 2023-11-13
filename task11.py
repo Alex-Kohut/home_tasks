50afbf0b95f2f89c1ce7a306acee6c92538b6832
Alex-Kohut <oleksandrkohut@gmail.com>
2023-11-13 11:07:27 +0200

    task 11

--- task11.py
+++ task11.py
@@ -1,5 +1,20 @@
 #ДЗ 11. Модифікувати калькулятор
 res = str(input('Do you want calculate something?'))
+
 while res == 'yes':
-    print(eval(input()))
-    res = str(input('Do you want continue?'))
+    number1 = int(input('Enter first num:'))
+    number2 = int(input('Enter second num:'))
+    math_operation = input('Enter math operation')
+    if math_operation == '+':
+        print(number1 + number2)
+    elif math_operation == '-':
+        print(number1 - number2)
+    elif math_operation == '*':
+        print(number1 * number2)
+    elif math_operation == '/' and number2 != 0:
+        print(number1 / number2)
+    else:
+        print('wrong operation!!!')
+    res = str(input('Do you want calculate something?'))
+else:
+    print('finish')
